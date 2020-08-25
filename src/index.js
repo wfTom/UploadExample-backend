cc0109cc500c0884f6104da4ab67e80f4644fe18
wfTom <wf_tom@hotmail.com.br>
2020-08-25 12:34:04 -0300

    add env port config of heroku

--- src/index.js
+++ src/index.js
@@ -8,20 +8,20 @@ const cors = require('cors');
 const app = express();
 
 //MongoDB Setup
-mongoose.connect(
-  process.env.MONGO_URL,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
+app.use(
+  '/files',
+  express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+);
 
 app.use(require('./routes'));
 
-app.listen(3010);
+app.listen(process.env.PORT || 3010);
